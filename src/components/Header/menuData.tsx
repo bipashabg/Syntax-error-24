9354423ba1008a41f13439c94eac5de3c9880938
SHANKAR2012 <shankar_n@es.iitr.ac.in>
2024-10-19 02:13:15 +0530

    Initial commit of frontend files

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,35 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Tokenize Asset",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Repay loan",
+    path: "/contact",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Apply for loan",
+    newTab: false,
+    
+  },
+];
+export default menuData;
---
+++ src/types/types.d.ts
@@ -0,0 +1,8 @@
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
+// This line is necessary to make TypeScript treat this file as a module
+export {};
